6b3286166680a993e10fba77768e288ceef19a56
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2025-06-21 16:03:00 +0700

    just commit

--- api/views.py
+++ api/views.py
@@ -1,22 +1,23 @@
 import os
 import tempfile
 
+from rest_framework import status
 from django.conf import settings
 from pydub import AudioSegment
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from shutil import copyfileobj
-
+import numpy as np
 from api.serilalizers import FileUploadSerializer
-from utils.loader import AudioModel
+from utils.loader import AudioModel, load_data, ImageModel
 
 model_path = os.path.join(settings.STATIC_PATH, 'model', 'model_audio.h5')
 audioModel = AudioModel(model_path)
 model_path = os.path.join(settings.STATIC_PATH, 'model', 'model_image.h5')
-imageModel = AudioModel(model_path)
+imageModel = ImageModel(model_path)
 
 allowed_file_extension = ['mp3', 'wav', 'mp4', 'm4a']
-
+data_dict = {3: 'lapar', 1: 'bersendawa atau sendawa', 2: 'tidak nyaman', 0: 'sakit perut', 4: 'lelah atau letih'}
 
 class PredictVideo(APIView):
     def get(self, request, *args, **kwargs):
@@ -38,25 +39,25 @@ class PredictAudio(APIView):
         uploaded_file = self.request.FILES['file']
         ext_name: str = uploaded_file.name.split('.')[-1]
 
-        if ext_name in self.allowed_file_extension:
+        if ext_name in allowed_file_extension:
             audio = AudioSegment.from_file(uploaded_file)
             with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_file:
                 audio.export(temp_file.name, format='wav')
                 temp_filename = temp_file.name
         else:
-            msg_list = [f".{x}" for x in self.allowed_file_extension]
+            msg_list = [f".{x}" for x in allowed_file_extension]
             msg = ' or '.join(msg_list)
             return Response({'error': f"File format extension not supported! Only {msg} are supported!"},
                             status=status.HTTP_403_FORBIDDEN)
 
         try:
-            data = load_data(temp_filename, self.inceptionv3)
+            data = load_data(temp_filename)
 
             pred = self.model.predict(data)
             prediction = np.argmax(pred)
             confidence = np.max(pred)
             confidence = 100.0 if confidence * 100 > 1.0 else confidence
-            pred_result = self.data_dict[prediction]
+            pred_result = data_dict[prediction]
 
         finally:
             if os.path.exists(temp_filename):
